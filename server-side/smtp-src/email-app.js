24d0a3af06c776a8fc6ca498161d54de6f87e873
hommym <herbertharthur80@gmail.com>
2024-02-29 01:49:48 -0800

    set up smtp server

---
+++ server-side/smtp-src/email-app.js
@@ -0,0 +1,72 @@
+// imorting  needed modules
+const SMTPServer = require("smtp-server").SMTPServer;
+const bcrypt= require("bcrypt")
+
+
+
+const server = new SMTPServer({
+logger:true ,
+secure:false,    
+onConnect(session, callback) {
+
+console.log(`New user with ip ${session.remoteAddress} has being connected`);
+return callback()
+},
+
+onData(stream, session, callback) {
+
+
+    stream.on("data",(data)=>{
+
+       console.log(data);
+    })
+
+
+    stream.on("end",()=>{
+
+
+        console.log("Message has been fully recieved");
+        console.log(session);
+
+        // use nodemailer to send message (not yet implemented)
+
+        callback()
+    })
+
+},
+
+async onAuth(auth, session, callback) {
+
+    
+
+    const{password,username}=auth
+
+    // checking if the connected client has an account on the server
+    if(!password||!username){
+        return callback(new Error("No password or username provided"))
+    }
+
+    const hashedPassword= bcrypt.hash(auth.password,10)
+    
+    // make a network request to http server to authenticate the user on the smtp server(not implemented)
+
+    callback(null, { user: username })
+
+
+}
+
+
+
+
+})
+
+
+
+
+
+
+server.listen(45, '127.0.0.1', () => {
+    console.log('SMTP server started on port 45')
+
+
+})
--- server-side/src/testAdmin.js
+++ server-side/src/testAdmin.js
@@ -13,3 +13,39 @@
 // id for test admin =65dd3f47c54da83b73f21bb5
 
 // passwor for gmail when using nodemailer=xcgf scrz anbg ofxu
+
+
+
+const nodeMailer=require("nodemailer")
+
+
+const transporter = nodeMailer.createTransport({
+    host: "localhost",
+    port: 45,
+    tls: {
+        rejectUnauthorized: false
+    },
+    auth: {
+        user: "HmmyMalone",
+        pass: "$2b$10$wPF4BBiDvizVDI7pRgn2kehYkvhYHMQ8Tv4ZrXxhUgSCjbmIYN0C2",
+      }
+  })
+
+
+  const mailOptions = {
+    from: "herbertharthur80@gmail.com",
+    to: "herebrthh@gmail",
+    subject: "Company Name:Email Confirmation",
+    text: `To confirm email click on this link`,
+  }
+
+
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
+  })
