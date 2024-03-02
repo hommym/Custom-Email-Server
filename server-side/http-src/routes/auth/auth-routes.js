dd43d36936c9bcc35959930e7e42ebf7a28df94f
hommym <herbertharthur80@gmail.com>
2024-03-02 10:05:56 -0800

    refactored codes in auth routes

--- server-side/http-src/routes/auth/auth-routes.js
+++ server-side/http-src/routes/auth/auth-routes.js
@@ -1,28 +1,27 @@
 // importing required modules 
 const express= require("express")
-const expressAsynHandler=require("express-async-handler")
+const expressAsyncHandler=require("express-async-handler")
 const {userSignUpController,logInController,emailConfirmationController,employeeSignUpController,loggedInController,resetPasswordController,changePasswordController}=require("./auth-controller.js")
 const adminChecker=require("../../middleware/admin-checker.js")
 const userChecker=require("../../middleware/userChecker.js")
 const emailSender=require("../../middleware/confirmation-email-sender.js")
 const verifyJWT=require("../../middleware/verify-jwt.js")
-const expressAsyncHandler = require("express-async-handler")
 const authRouter=express.Router()
 
 
-authRouter.post("/user/sign-up",expressAsynHandler(userSignUpController),expressAsynHandler(emailSender))
-authRouter.get("/user/send-confirmation-email",expressAsynHandler(emailSender))
+authRouter.post("/user/sign-up",expressAsyncHandler(userSignUpController),expressAsyncHandler(emailSender))
+authRouter.get("/user/send-confirmation-email",expressAsyncHandler(emailSender))
 
-authRouter.post("/employee/sign-up",expressAsynHandler(verifyJWT),expressAsynHandler(adminChecker),expressAsynHandler(employeeSignUpController),expressAsynHandler(emailSender))
+authRouter.post("/employee/sign-up",expressAsyncHandler(verifyJWT),expressAsyncHandler(adminChecker),expressAsyncHandler(employeeSignUpController),expressAsyncHandler(emailSender))
 
-authRouter.get("/login",expressAsynHandler(userChecker),expressAsynHandler(logInController))
-authRouter.get("/logged-in",expressAsynHandler(verifyJWT),expressAsynHandler(userChecker),expressAsynHandler(loggedInController))
+authRouter.get("/login",expressAsyncHandler(userChecker),expressAsyncHandler(logInController))
+authRouter.get("/logged-in",expressAsyncHandler(verifyJWT),expressAsyncHandler(userChecker),expressAsyncHandler(loggedInController))
 
-authRouter.put("/reset-password",expressAsyncHandler(userChecker),expressAsynHandler(resetPasswordController),expressAsynHandler(emailSender))
+authRouter.put("/reset-password",expressAsyncHandler(userChecker),expressAsyncHandler(resetPasswordController),expressAsyncHandler(emailSender))
 
-authRouter.put("/change-password",expressAsynHandler(verifyJWT),expressAsynHandler(userChecker),expressAsynHandler(changePasswordController))
+authRouter.put("/change-password",expressAsyncHandler(verifyJWT),expressAsyncHandler(userChecker),expressAsyncHandler(changePasswordController))
 
-authRouter.get("/email-confirmation",expressAsynHandler(emailConfirmationController))
+authRouter.get("/email-confirmation",expressAsyncHandler(emailConfirmationController))
 
 
 
