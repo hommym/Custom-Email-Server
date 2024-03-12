005549c909c2acc5be09eec19637935ca6c3015b
FosterSOAsare <fostersoasare@gmail.com>
2024-03-12 11:54:17 +0000

    Contact us page UI completed

--- client-side/src/pages/contact-us.tsx
+++ client-side/src/pages/contact-us.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Link from "next/link";
+
+import Staticpage from "@/components/layouts/Staticpage";
+import PrimaryButton from "@/components/atoms/PrimaryButton";
 
 const contactUs = () => {
-	return <div>Contact</div>;
+	return (
+		<Staticpage>
+			<main className="flex flex-col items-center justify-center py-24">
+				<h3 className="text-deep-text text-4xl mb-8 font-bold">Let's talk</h3>
+				<p className="mb-3">
+					For most issues, we've prepared our in {""}
+					<Link href="/" className="text-deep-text font-bold hover:underline">
+						Help Center
+					</Link>{" "}
+					.
+				</p>
+
+				<p className="text-center w-1/2 leading-[30px]">If you still have some questions, don't worry - we have the answers. You can reach out to us directly by completing the form below.</p>
+
+				<section className="w-full h-auto">
+					<div className="w-full max-w-6xl mx-auto rounded-[25px] shadow-md h-auto bg-[#F8F8FC] mt-12 py-24">
+						<form action="" className="w-[70%] mx-auto ">
+							<div className="flex items-center justify-between gap-4">
+								<div className="w-full">
+									<label htmlFor="" className="block mb-[5px] text-deep-text font-medium text-sm">
+										Name <span className="text-red-400">*</span>
+									</label>
+									<input className="w-full h-12 px-3 border-[1px] rounded-[5px] " type="text" />
+								</div>
+								<div className="w-full">
+									<label htmlFor="" className="block mb-[5px] text-deep-text font-medium text-sm">
+										Email <span className="text-red-400">*</span>
+									</label>
+									<input className="w-full h-12 px-3 border-[1px] rounded-[5px] " type="text" />
+								</div>
+							</div>
+
+							<div className="mt-8">
+								<label htmlFor="" className="block mb-[5px] text-deep-text font-medium text-sm">
+									Message <span className="text-red-400">*</span>
+								</label>
+								<textarea name="" className="w-full h-48 border-[1px] resize-none rounded-[5px]"></textarea>
+							</div>
+
+							<PrimaryButton text="Send" sx="!w-[150px] !h-[60px] py-4 !font-bold !bg-deep-text !mt-8" />
+						</form>
+					</div>
+				</section>
+			</main>
+		</Staticpage>
+	);
 };
 
 export default contactUs;
--- client-side/src/pages/dashboard/index.tsx
+++ client-side/src/pages/dashboard/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 import Mainpage from "@/components/layouts/Mainpage";
+import PrimaryButton from "@/components/atoms/PrimaryButton";
 
 const index = () => {
-	return <Mainpage>index</Mainpage>;
+	return (
+		<Mainpage>
+			<main>
+				<h3 className="text-3xl mb-4">Hello Ty,</h3>
+				<p className="font-bold opacity-80">It's good to have you here!</p>
+
+				<div className="bg-[#EDEEFF] p-5 border-[1px] mt-4 flex items-center justify-between">
+					<div className="w-full">
+						<h3 className="mb-4">Boost your email deliverability!</h3>
+						<p className="text-sm">
+							If you want your emails to always reach the inbox - validate your sending domain. It will help email providers recognize your emails as trustworthy and deliver them to your
+							subscribers' inboxes. Learn more
+						</p>
+					</div>
+					<PrimaryButton text="Go to Domains" sx="!w-[200px] !font-medium" href="/dashboard/domains" />
+				</div>
+			</main>
+		</Mainpage>
+	);
 };
 
 export default index;
