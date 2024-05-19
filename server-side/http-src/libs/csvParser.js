db210f3b138034b89fae86e8a4952e451f4a47e6
herberth arthur <herbertharthur80@gmail.com>
2024-05-19 11:56:56 +0000

    Code Refactor

--- server-side/http-src/libs/csvParser.js
+++ server-side/http-src/libs/csvParser.js
@@ -2,7 +2,7 @@ const csvParser = require("csv-parser");
 const { Readable } = require("stream");
 const addressFilter = require("../../helperTools/emailadressFilter.js");
 
-const csvToArray = async (fileBuffer,req) => {
+const csvToArray = async (fileBuffer, req, filterFlag = true) => {
   let results = [];
   const stream = new Readable.from(fileBuffer.toString("utf-8"));
 
@@ -12,7 +12,12 @@ const csvToArray = async (fileBuffer,req) => {
       results.push(data);
     })
     .on("end", async () => {
-      results = await addressFilter(results[0].split(","), req);
+      if (filterFlag) {
+        results = await addressFilter(results[0].split(","), req);
+      } else {
+        results = results[0].split(",");
+      }
+
       return { emailAddresses: results, addressCount: results.length };
       // res.status(200).json({ emailAddresses: results, addressCount: results.length });
     });
