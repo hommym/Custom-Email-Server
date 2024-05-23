04cd05ce83e528e0707d9db5259a9c350f0ca6b7
herberth arthur <herbertharthur80@gmail.com>
2024-05-23 12:35:14 +0000

    Temporary change

--- server-side/helperTools/emailRouting.js
+++ server-side/helperTools/emailRouting.js
@@ -24,10 +24,11 @@ const emailRouter = async (emailQueue, eventEmmitter) => {
 
         if (isRecipientServerOnPort25) {
           // using postfix to handle private mail server addresses if it is on port 25
-          addressesForThirdPartySender.push(address);
+          // addressesForThirdPartySender.push(address);
         } else {
           addressesForDefaultSender.push(address);
         }
+         addressesForThirdPartySender.push(address);
       } else {
         console.log("Invalid email address");
         // add code for removing such email addresses from users contacts(Not implemented yet)
