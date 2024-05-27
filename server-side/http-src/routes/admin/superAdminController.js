6b9a3d25d483d602c98bd7b9a95bb649d6e61520
herberth arthur <herbertharthur80@gmail.com>
2024-05-27 16:05:23 +0000

    Added code to allow a superAdmin to create a super admin account.

--- server-side/http-src/routes/admin/superAdminController.js
+++ server-side/http-src/routes/admin/superAdminController.js
@@ -80,7 +80,7 @@ const bulkEmailUploadController = asyncHandler(async (req, res) => {
 const allBulkEmailController = asyncHandler(async (req, res) => {});
 
 const adminCreatorController = asyncHandler(async (req, res) => {
-  const { lastname, firstname, password, email, hasSpecialAccess } = req.body;
+  const { lastname, firstname, password, email, hasSpecialAccess, role } = req.body;
 
   // checking all the needed data for creating account is present
 
@@ -107,6 +107,10 @@ const adminCreatorController = asyncHandler(async (req, res) => {
 
   if (hasSpecialAccess) {
     documentToSave.hasSpecialAccess = true;
+  }
+
+  if(role){
+     documentToSave.role = "superAdmin";
   }
 
   // saving data in database
