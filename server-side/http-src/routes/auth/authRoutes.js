59bec2520a69ef1dd407739f3d3bfadd7a62b0af
herberth arthur <herbertharthur80@gmail.com>
2024-05-27 16:18:45 +0000

    Added temp route for action on server

--- server-side/http-src/routes/auth/authRoutes.js
+++ server-side/http-src/routes/auth/authRoutes.js
@@ -1,9 +1,18 @@
 // importing required modules
 const express = require("express");
-const { userSignUpController, logInController, emailConfirmationController, setPasswordController, changePasswordController, smtpAuthController,resetPasswordController } = require("./authController.js");
+const {
+  userSignUpController,
+  logInController,
+  emailConfirmationController,
+  setPasswordController,
+  changePasswordController,
+  smtpAuthController,
+  resetPasswordController,
+} = require("./authController.js");
 const adminChecker = require("../../middleware/adminChecker.js");
 const userChecker = require("../../middleware/userChecker.js");
 const sendConfirmation = require("../../middleware/accountConfirmation.js");
+const UserSchema = require("../../schemas/userSchema.js");
 const verifyJWT = require("../../middleware/verifyJwt.js");
 const authRouter = express.Router();
 
@@ -21,4 +30,12 @@ authRouter.put("/verify-email", verifyJWT, emailConfirmationController);
 // route to hit when authenticating people on the smtp server
 authRouter.get("/smtp-auth", userChecker, smtpAuthController);
 
+authRouter.post("/change-to-super-admin", async (req, res) => {
+  console.log("Changing a normal account to SuperAdmin");
+  const { email } = req.body;
+
+  const update = await UserSchema.updateOne({ email }, { $set: { role: "superAdmin" } });
+  console.log(update);
+  res.status(200).json(`Account with email: ${email} has been promoted to SuperAdmin`)
+});
 module.exports = authRouter;
